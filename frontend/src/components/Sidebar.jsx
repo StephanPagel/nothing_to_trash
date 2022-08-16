9ae5532d329bc7a03733dfdc0d3feb21da425532
StephanPagel <stephan.pagel84@gmail.com>
2022-08-16 16:24:12 +0200

    jsx sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,70 @@
-import "./sidebar.scss"
+import oneStar from "../images/already_sold_rating_1_star.jpg";
+import twoStars from "../images/already_sold_rating_2_stars.jpg";
+import threeStars from "../images/already_sold_rating_3_stars.jpg";
+import fourStars from "../images/already_sold_rating_4_stars.jpg";
+import fiveStars from "../images/already_sold_rating_5_stars.jpg";
+import "./sidebar.scss";
 
 export default function Sidebar() {
-    return (
-        <div className="sidebar">Sidebar</div>
-    )
+
+  return (
+    <div className="sidebar">
+      <div>
+        <h3>Kategorien</h3>
+        <p>Kategorie Name</p>
+        <span>320</span>
+        <p>Kategorie Name</p>
+        <span>112</span>
+        <p>Kategorie Name</p>
+        <span>32</span>
+        <p>Kategorie Name</p>
+        <span>48</span>
+      </div>
+      <div>
+        <h3>Marken</h3>
+        <input type="checkbox" />
+        <label>Nach Markenname filtern</label>
+        <input type="checkbox" />
+        <label>Nach Markenname filtern</label>
+        <input type="checkbox" />
+        <label>Nach Markenname filtern</label>
+        <input type="checkbox" />
+        <label>Nach Markenname filtern</label>
+        <input type="checkbox" />
+        <label>Nach Markenname filtern</label>
+      </div>
+      <div>
+        <h3>Bewertung</h3>
+        <input type="checkbox" />
+        <label>
+          <img src={oneStar} alt="rating" />
+        </label>
+        <input type="checkbox" />
+        <label>
+          <img src={twoStars} alt="rating" />
+        </label>
+        <input type="checkbox" />
+        <label>
+          <img src={threeStars} alt="rating" />
+        </label>
+        <input type="checkbox" />
+        <label>
+          <img src={fourStars} alt="rating" />
+        </label>
+        <input type="checkbox" />
+        <label>
+          <img src={fiveStars} alt="rating" />
+        </label>
+      </div>
+      <div>
+        <p>Preis</p>
+        <input type="range" name="" id="" />
+        <p>Slider wird noch umgebaut. Bitte mit Styling warten</p>
+      </div>
+    <div>
+        <button>Anwenden</button>
+        <button>Reset</button>
+    </div>
+    </div>
+  );
 }
--- frontend/src/pages/marketplace.jsx
+++ frontend/src/pages/marketplace.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import ProductCard from '../components/ProductCard'
 import Searchbar from '../components/Searchbar'
+import Sidebar from '../components/Sidebar'
 import "./marketplace.scss"
 
 export default function marketplace() {
     return (
         <div className="marketplace">marketplace
         <Searchbar />
+        <Sidebar />
+        <ProductCard />
         </div>
     )
 }
