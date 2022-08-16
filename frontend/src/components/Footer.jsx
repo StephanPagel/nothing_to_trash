8ebec036962a94d46d0967e6b80e628317bdc905
StephanPagel <stephan.pagel84@gmail.com>
2022-08-16 16:55:11 +0200

    jsx files renamed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,14 +5,16 @@ import { BsFacebook } from 'react-icons/bs'
 
 export default function Footer() {
     return (
-        <div className="footer">
-            <div className="footer_container">
-                <AiOutlineTwitter />
-                <BsFacebook />
-                <AiFillInstagram />
-                <p>COPYRIGHT nothingtotrash 2021 -</p>
-                <span>TERMS & CONDITIONS PRIVACY POLICY</span>
-            </div>
+      <div className="footer">
+        <h3>nothingtotrash - eine online Plattform um Müll zu vermeiden.</h3>
+        <button>Registriere Dich</button>
+        <div className="footer_container">
+          <AiOutlineTwitter />
+          <BsFacebook />
+          <AiFillInstagram />
+          <p>COPYRIGHT nothingtotrash 2021 -</p>
+          <span>TERMS & CONDITIONS PRIVACY POLICY</span>
         </div>
+      </div>
     );
 }
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -3,8 +3,17 @@ import './home.scss';
 
 export default function home() {
     return (
-        <div className="home">
-            <div>HALOO</div>
-        </div>
-    )
+      <div className="home">
+        <h1>Hilf mit, die Umwelt zu schützen</h1>
+        <p>
+          Abfälle bedrohen Vögel, Delfine und Co. Mehr als zehn Millionen Tonnen
+          Abfälle gelangen jährlich in die Ozeane. Sie kosten Abertausende
+          Meerestiere das Leben. Seevögel verwechseln Plastik mit natürlicher
+          Nahrung, Delfine verfangen sich in alten Fischernetzen. Hilf mit Müll
+          zu reduzieren und trashnothing.
+        </p>
+        <button>Starte jetzt!</button>
+        
+      </div>
+    );
 }
