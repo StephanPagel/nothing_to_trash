ea5a0d4ae9dabc4dd1ce1ee1d59bf176e084ca99
werthmannsupercode <werthmannsupercode@gmail.com>
2022-08-16 17:24:43 +0200

    Post route steht

--- backend/src/database/products-dao.js
+++ backend/src/database/products-dao.js
@@ -3,9 +3,25 @@ const { injectDB } = require("./getDB");
 
 const productsCollectionName = "products";
 
-async function insertNewProduct(productInfo) {
-  const db = await getDB();
-  return db.collection(productsCollectionName).insertOne(productInfo);
+function addNewProduct(productInfo) {
+
+  return new Promise((resolve, reject) => {
+    injectDB()
+      .then(db => db.collection(productsCollectionName).insertOne(productInfo))
+      .then(result => {
+        if (result.acknowledged === true && result.insertedId) {
+          return resolve({
+            _id: result.insertedId,
+            ...productInfo,
+          })
+        } else {
+          // result könnte ein error sein, daher reject...
+          return reject(result)
+        }
+      }).catch((err) => reject(err))
+  })
+  // const db = await injectDB();
+  // return db.collection(productsCollectionName).insertOne(productInfo);
 }
 
-module.exports = { insertNewProduct };
+module.exports = { addNewProduct };
--- backend/src/routes/products-router.js
+++ backend/src/routes/products-router.js
@@ -1,26 +1,26 @@
 const express = require("express");
-const { createProduct } = require("../use-cases/createProduct");
+const { createProduct } = require("./../use-cases/createProduct");
 
 const productsRouter = express.Router();
 
 productsRouter.post("/addnewProduct", (req, res) => {
-  if (!req.body) {
-    res.status(400).json({ error: "Please include a item." });
-    return;
-  }
+    if (!req.body) {
+        res.status(400).json({ error: "Please include a item." });
+        return;
+    }
 
-  const newItem = {
-    Title: req.body.title,
-    Description: req.body.desc,
-    Price: "$" + req.body.price,
-  };
+    const newItem = {
+        title: req.body.title,
+        description: req.body.description,
+        price: "$" + req.body.price
+    };
 
-  createProduct(newItem)
-    .then((addedItem) => res.status(201).json(addedItem))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: "Failed to add item to database." });
-    });
+    createProduct(newItem)
+        .then((addedItem) => res.status(201).json(addedItem))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ error: "Failed to add item to database." });
+        });
 });
 
 module.exports = { productsRouter };
