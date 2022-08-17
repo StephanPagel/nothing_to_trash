5b0be4cc890305e57775a91b2eadd752d62612ea
StephanPagel <stephan.pagel84@gmail.com>
2022-08-17 12:13:54 +0200

    jsx alreadySold productDetails wishList

--- frontend/src/pages/wishlist.jsx
+++ frontend/src/pages/wishlist.jsx
@@ -1,11 +1,15 @@
+import ProductCard from '../components/ProductCard';
 import Searchbar from '../components/Searchbar'
+import Sidebar from '../components/Sidebar';
 import "./wishlist.scss"
 
 export default function wishlist() {
     return (
-        <div className="wishlist">
-            <Searchbar />
-            wishlist
-        </div>
-    )
+      <div className="wishlist">
+        <h1>Diese Artikel hättest du gerne</h1>
+        <Searchbar />
+        <Sidebar />
+        <ProductCard />
+      </div>
+    );
 }
