43b1e54e52abf37cd41c079b189fcdcb6325c4e7
StephanPagel <stephan.pagel84@gmail.com>
2022-08-17 16:16:13 +0200

    setproduct states added

--- frontend/src/pages/setProduct.jsx
+++ frontend/src/pages/setProduct.jsx
@@ -1,4 +1,14 @@
 import "./set_product.scss";
+import { useState } from "react";
+
+const [title, setTitle] = useState("");
+const [description, setDescription] = useState("");
+const [amount, setAmount] = useState("");
+const [price, setPrice] = useState("");
+const [zip, setZip] = useState("");
+const [city, setCity] = useState("");
+const [name, setName] = useState("");
+const [phone, setPhone] = useState("")
 
 const setProduct = () => {
   return (
@@ -24,18 +34,38 @@ const setProduct = () => {
       <input type="radio" name="delivery" id="deliveryNo" />
       <label>Nein</label>
       <label>Titel der Anzeige:</label>
-      <input type="text" name="title" id="title" />
+      <input
+        type="text"
+        name="title"
+        id="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
       <label>Beschreibung:</label>
       <textarea
         name="description"
         id="description"
         cols="50"
         rows="5"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
       ></textarea>
       <label>Anzahl:</label>
-      <input type="number" name="amount" id="amount" />
+      <input
+        type="number"
+        name="amount"
+        id="amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
       <label>Preis:</label>
-      <input type="number" name="price" id="price" />
+      <input
+        type="number"
+        name="price"
+        id="price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
       <label>EUR</label>
       <input
         type="radio"
@@ -69,7 +99,15 @@ const setProduct = () => {
         <option value="5">Kategorie 5</option>
       </select>
       <label>PLZ/Ort*</label>
-      <input type="text" name="zip" id="zip" required placeholder="PLZ" />
+      <input
+        type="text"
+        name="zip"
+        id="zip"
+        required
+        placeholder="PLZ"
+        value={zip}
+        onChange={(e) => setZip(e.target.value)}
+      />
       <input type="text" name="city" id="city" required placeholder="Ort" />
       <label>Straße/Nr.*</label>
       <input
@@ -78,9 +116,19 @@ const setProduct = () => {
         id="street"
         required
         placeholder="Straße/Nr."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
       />
       <label>Name*</label>
-      <input type="text" name="name" id="name" required placeholder="Name" />
+      <input
+        type="text"
+        name="name"
+        id="name"
+        required
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label>Telefonnummer</label>
       <input
         type="number"
@@ -88,6 +136,8 @@ const setProduct = () => {
         id="phone"
         required
         placeholder="Telefon"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
       />
       <button type="submit">Produkt einstellen</button>
     </form>
