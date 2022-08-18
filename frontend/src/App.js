b6f6a419396eca0b7c6f465e3ad6fac4a6d29df7
githubsupercode <alex.supercode@gmail.com>
2022-08-18 16:58:39 +0200

    case

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
 import Marketplace from "./pages/Marketplace";
 import SetProduct from "./pages/SetProduct";
 import Wishlist from "./pages/Wishlist";
@@ -12,19 +11,12 @@ import AlreadySold from "./pages/AlreadySold";
 import ProductDetail from "./pages/ProductDetail";
 import AboutUs from "./pages/AboutUs";
 
-console.log("%c****************", 'color:yellow');
-console.log("%cNOTHING TO TRASH", 'color:white');
-console.log("%c© Emre, Stephan, Alex, Michaela und Elias / SuperCode 2022", 'color:violet');
-console.log("%c****************", 'color:yellow');
-=======
-import Marketplace from "./pages/marketplace";
-import SetProduct from "./components/SetProductForm";
-import Wishlist from "./pages/wishlist";
-import AlreadySold from "./pages/alreadySold";
-import ProductDetail from "./pages/productDetail";
-import AboutUs from "./pages/aboutUs";
->>>>>>> bcbf559c01b1a2ba3a59e1cd4e740bb88fc871e4
-
+console.log("%c****************", "color:yellow");
+console.log("%cNOTHING TO TRASH", "color:white");
+console.log(
+  "%c© Emre, Stephan, Alex, Michaela und Elias / SuperCode 2022",
+  "color:violet"
+);
 
 function App() {
   return (
