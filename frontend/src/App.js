27fccfcd081c29250f8c5699432e0d4bbbf2b150
StephanPagel <stephan.pagel84@gmail.com>
2022-08-19 11:44:57 +0200

    SetProductForm updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,9 @@ import Wishlist from "./pages/Wishlist";
 import AlreadySold from "./pages/AlreadySold";
 import ProductDetail from "./pages/ProductDetail";
 import AboutUs from "./pages/AboutUs";
+import {apiBaseUrl} from '../src/api'
+import { useState, useEffect } from "react";
+
 
 console.log("%c****************", 'color:yellow');
 console.log("%cNOTHING TO TRASH", 'color:white');
@@ -18,13 +21,23 @@ console.log("%c****************", 'color:yellow');
 
 
 function App() {
+
+  const [allProducts, setAllProducts] = useState([])
+
+useEffect(() => {
+  fetch(`${apiBaseUrl}allproducts`)
+    .then((allProducts) => allProducts.json())
+    .then((productsArray) => setAllProducts(productsArray))
+    .catch((err) => console.log(err));
+}, [])
+
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
-        <Route path="marketplace" element={<Marketplace />} />
+        <Route path="marketplace" element={<Marketplace allProducts={allProducts} />} />
         <Route path="set_product" element={<SetProduct />} />
         <Route path="product_details" element={<ProductDetail />} />
         <Route path="sold" element={<AlreadySold />} />
--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,23 +1,24 @@
-import "./productCard.scss"
-import chair from '../images/marketplace_chair.jpg'
+import "./productCard.scss";
+import chair from "../images/marketplace_chair.jpg";
 
-export default function ProductCard() {
-    return (
-        <div className="product-card">
-            <img src={chair} alt="product" />
-            <h3>45.00 EUR</h3>
-            <h3>Produkt Titel</h3>
-            <h4>Kleine Produktbeschreibung</h4>
-            <p>Zustand</p>
-            <span>Wie neu</span>
-            <p>Marke</p>
-            <p>Grocery Tarm Fields</p>
-            <p>Lieferung</p>
-            <p>Jahr</p>
-            <p>Auf Lager</p>
-            <span>320 Stk</span>
-            <button>Details ＞</button>
-            <button>♡ Auf die Wunschliste</button>
-        </div>
-    )
+export default function ProductCard(props) {
+  return (
+
+    <div className="product-card">
+      <img src={chair} alt="product" />
+      <h3>45.00 EUR</h3>
+      <h3>Produkt Titel</h3>
+      <h4>Kleine Produktbeschreibung</h4>
+      <p>Zustand</p>
+      <span>Wie neu</span>
+      <p>Marke</p>
+      <p>Grocery Tarm Fields</p>
+      <p>Lieferung</p>
+      <p>Jahr</p>
+      <p>Auf Lager</p>
+      <span>320 Stk</span>
+      <button>Details ＞</button>
+      <button>♡ Auf die Wunschliste</button>
+    </div>
+  );
 }
