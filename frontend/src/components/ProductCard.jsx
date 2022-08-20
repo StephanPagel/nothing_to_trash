8fdc023da65cccc0a958d62c93ccaf8e24f539e1
StephanPagel <stephan.pagel84@gmail.com>
2022-08-20 05:03:42 +0200

    ProductCard manually merged

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,58 +1,38 @@
-
 import "./productCard.scss";
-import chair from "../images/marketplace_chair.jpg";
 import { apiBaseUrl } from "../api";
 
 export default function ProductCard(props) {
   return (
-    <div className="product-card">
-      <img
-        src={props.filename && `${apiBaseUrl}/${props.filename}`}
-        alt="product"
-      />
-      <h3>{props.price}</h3>
-      <h3>{props.title}</h3>
-      <h4>{props.descriptionShort}</h4>
-      <p>Zustand</p>
-      <span>{props.condition}</span>
-      <p>Marke</p>
-      <p>{props.brand}</p>
-      <p>Lieferung</p>
-      <p>{props.delivery === "deliveryYes" ? "Ja" : "Nein"}</p>
-      <p>Auf Lager</p>
-      <span>{props.amount}</span>
-      <button>Details ＞</button>
-      <button>♡ Auf die Wunschliste</button>
-
-export default function ProductCard() {
-  return (
     <div className="product_card">
       <div className="product">
-        <img src={chair} alt="product" />
+        <img
+          src={props.filename && `${apiBaseUrl}/${props.filename}`}
+          alt="product"
+        />
         <div className="product_title">
-          <h3>45.00 EUR</h3>
-          <h3>Produkt Titel</h3>
-          <h4>Produktbeschreibung</h4>
+          <h3>{props.price}</h3>
+          <h3>{props.title}</h3>
+          <h4>{props.descriptionShort}</h4>
         </div>
         <div className="product_details">
           <tr>
             <td>Zustand</td>
             <td>
-              <span>Wie neu</span>
+              <span>{props.condition}</span>
             </td>
           </tr>
           <tr>
             <td>Marke</td>
-            <td>Grocy</td>
+            <td>{props.brand}</td>
           </tr>
           <tr>
             <td>Lieferung</td>
-            <td>30.06.2070</td>
+            <td>{props.delivery === "deliveryYes" ? "Ja" : "Nein"}</td>
           </tr>
           <tr>
             <td>Auf Lager</td>
             <td>
-              <span>300 Stk</span>
+              <span>{props.amount}</span>
             </td>
           </tr>
           <button>Details ＞</button>
