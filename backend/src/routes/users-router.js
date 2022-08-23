481d16233f3d872ee0acb78c7ac5f7ef9b1295e8
githubsupercode <alex.supercode@gmail.com>
2022-08-23 16:06:58 +0200

    sidebar

--- backend/src/routes/users-router.js
+++ backend/src/routes/users-router.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { registerUser } = require("../use-cases/register-user");
 const { loginUser } = require("../use-cases/login-user");
 const { showUserProfil } = require("../use-cases/showUserProfil");
-const { addProducttoUsersWishlist } = require("./../use-cases/addProducttoUserWishlist");
+const {
+  addProducttoUsersWishlist,
+} = require("./../use-cases/addProducttoUserWishlist");
 const { makeDoAuthMiddleware } = require("./../auth/doAuthMiddleware");
 
 const doAuthMiddleware = makeDoAuthMiddleware("access");
@@ -35,43 +37,34 @@ usersRouter.post("/login", async (req, res) => {
     res.json({ accessToken, refreshToken });
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: err.toString() || "Internal Server Error." });
-  };
-});
-
-usersRouter.post("/refreshtoken", doRefreshTokenMiddleware, async (req, res) => {
-  try {
-    const userId = req.userClaims.sub;
-    const accessToken = await refreshUserToken({ userId });
-    res.json({ token: accessToken });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: err.toString() || "Internal Server Error.",
-    });
+    res
+      .status(500)
+      .json({ message: err.toString() || "Internal Server Error." });
   }
 });
+
+usersRouter.post(
+  "/refreshtoken",
+  doRefreshTokenMiddleware,
+  async (req, res) => {
+    try {
+      const userId = req.userClaims.sub;
+      const accessToken = await refreshUserToken({ userId });
+      res.json({ token: accessToken });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: err.toString() || "Internal Server Error.",
+      });
+    }
+  }
+);
 
 usersRouter.get("/userprofil", doAuthMiddleware, async (req, res) => {
   try {
     const userId = req.userClaims.sub;
     const userProfile = await showUserProfil({ userId });
     res.json(userProfile);
-  } catch (err) {
-    console.log(err);
-
-    res.status(500).json({
-      message: err.toString() || "Internal Server Error.",
-    });
-  }
-});
-
-usersRouter.put("addProducttoWishlist/:id", async (req, res) => {
-  try {
-    const userId = req.userClaims.sub;
-    const productId = req.params.id;
-    const usersWishlist = await addProducttoUsersWishlist({ userId, productId });
-    res.json(usersWishlist);
   } catch (err) {
     console.log(err);
 
